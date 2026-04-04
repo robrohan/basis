4fb0ed060e1190ae81b2d786d3d175e2bb8bdc95
Rob Rohan <rob@therohans.com>
2026-04-04 20:36:59 +1300

    Adding asserts

--- test_data/tensor_test.lisp
+++ test_data/tensor_test.lisp
@@ -11,7 +11,7 @@
 					; => 0
 
 					; vector literal
-(assert (print [1 2 3]) [1 2 3])
+(assert [1 2 3] [1 2 3])
 					; => [1 2 3]
 
 (print "-- rank --")
@@ -44,52 +44,56 @@
 					; => ()
 
 (print "-- slice --")
-(print (slice [10 20 30] 0))
+(assert (slice [10 20 30] 0) 10)
 					; => 10
 
-(print (slice [10 20 30] 1))
+(assert (slice [10 20 30] 1) 20)
 					; => 20
 
-(print (slice [10 20 30] 2))
+(assert (slice [10 20 30] 2) 30)
 					; => 30
 
 (print "-- element-wise arithmetic --")
-(assert (print (+ [1 2 3] [4 5 6])) [5 7 9])
+(assert (+ [1 2 3] [4 5 6]) [5 7 9])
 					; => [5 7 9]
 
-(assert (print (- [10 20 30] [1 2 3])) [9 18 27])
+(assert (- [10 20 30] [1 2 3]) [9 18 27])
 					; => [9 18 27]
 
-(assert (print (* [2 3 4] [1 2 3])) [2 6 12])
+(assert (* [2 3 4] [1 2 3]) [2 6 12])
 					; => [2 6 12]
 
-(assert (print (/ [10 20 30] [2 4 5]))  [5 5 6])
+(assert (/ [10 20 30] [2 4 5]) [5 5 6])
 					; => [5 5 6]
 
 (print "-- scalar broadcast --")
-(assert (print (+ [1 2 3] 10) [11 12 13]))
+(assert (+ [1 2 3] 10) [11 12 13])
 					; => [11 12 13]
 
-(assert (print (* [1 2 3] 2)) [2 4 6])
+(assert (* [1 2 3] 2) [2 4 6])
 					; => [2 4 6]
 
-(assert (print (- [10 20 30] 5)) [5 15 25])
+(assert (- [10 20 30] 5) [5 15 25])
 					; => [5 15 25]
 
-(print (/ [10 20 30] 10))
+(assert (/ [10 20 30] 10) [1 2 3])
 					; => [1 2 3]
 
 (print "-- tensor in define / lambda --")
+
 (define v [3 1 4 1 5])
-(assert (shape v) 5)
+
+(assert (shape v) [5])
 					; => [5]
 
 (assert (slice v 2) 4)
 					; => 4
 
-(assert (print (+ v [1 1 1 1 1]) [4 2 5 2 6]))
+(assert (+ v [1 1 1 1 1]) [4 2 5 2 6])
 					; => [4 2 5 2 6]
 
 (print "-- nested expressions --")
-(assert (print (+ [1 2 3] (* [2 2 2] [3 4 5]))) [7 10 13])
+(assert (+ [1 2 3]
+	   (* [2 2 2] [3 4 5]))
+	[7 10 13])
 					; => [7 10 13]
